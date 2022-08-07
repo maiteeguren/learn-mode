041dd305af35072515674cd5f52133fb9c863f40
Maite <maite.brown@metigy.com>
2022-08-07 11:53:51 +1000

    restore constants

--- src/constants.js
+++ src/constants.js
@@ -14,39 +14,39 @@ export const BUNDLE_QUESTIONS = [
         text: 'Who was the last Tsar of Russia?',
         answer: 'Nicholas II'
     },
-    // {
-    //     id: 4,
-    //     text: 'How many hearts does an octopus have?',
-    //     answer: '3'
-    // },
-    // {
-    //     id: 5,
-    //     text: 'What is the capital of Ireland?',
-    //     answer: 'Dublin'
-    // },
-    // {
-    //     id: 6,
-    //     text: 'How many stars are on the Chinese flag?',
-    //     answer: '5'
-    // },
-    // {
-    //     id: 7,
-    //     text: 'Where did sushi originate?',
-    //     answer: 'China'
-    // },
-    // {
-    //     id: 8,
-    //     text: 'Pink Ladies and Granny Smiths are types of what fruit?',
-    //     answer: 'Apples'
-    // },
-    // {
-    //     id: 9,
-    //     text: 'What nut is in the middle of a Ferrero Rocher?',
-    //     answer: 'Hazelnut'
-    // },
-    // {
-    //     id: 10,
-    //     text: 'What does IPA stand for?',
-    //     answer: 'Indian Pale Ale'
-    // }
+    {
+        id: 4,
+        text: 'How many hearts does an octopus have?',
+        answer: '3'
+    },
+    {
+        id: 5,
+        text: 'What is the capital of Ireland?',
+        answer: 'Dublin'
+    },
+    {
+        id: 6,
+        text: 'How many stars are on the Chinese flag?',
+        answer: '5'
+    },
+    {
+        id: 7,
+        text: 'Where did sushi originate?',
+        answer: 'China'
+    },
+    {
+        id: 8,
+        text: 'Pink Ladies and Granny Smiths are types of what fruit?',
+        answer: 'Apples'
+    },
+    {
+        id: 9,
+        text: 'What nut is in the middle of a Ferrero Rocher?',
+        answer: 'Hazelnut'
+    },
+    {
+        id: 10,
+        text: 'What does IPA stand for?',
+        answer: 'Indian Pale Ale'
+    }
 ]
